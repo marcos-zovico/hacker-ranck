d24e1ac65eaac2044a509dcc3d7e0f55af194d13
Marcos A. G. Souza <marcos.a.g.souza@outlook.com>
2024-08-28 18:45:57 -0300

    java instanceof keyword

---
+++ src/test/java/hackerrank/java/oop/JavaInstanceofKeywordTest.java
@@ -0,0 +1,35 @@
+package hackerrank.java.oop;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JavaInstanceofKeywordTest {
+
+    private static final String IN = """
+            5
+            Student
+            Student
+            Rockstar
+            Student
+            Hacker""";
+
+    private static final String OUT = "3 1 1";
+
+    @BeforeAll
+    static void setup() {
+        System.setIn(new ByteArrayInputStream(IN.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    void runCode() throws Exception {
+        String actual = tapSystemOut(JavaInstanceofKeyword::runCode);
+        assertEquals(OUT, actual.strip());
+    }
+}
