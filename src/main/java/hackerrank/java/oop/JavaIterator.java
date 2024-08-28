f3cff145e6336fd8d4fde92c8a964183e98302e4
Marcos A. G. Souza <marcos.a.g.souza@outlook.com>
2024-08-28 19:28:47 -0300

    java iterator

---
+++ src/main/java/hackerrank/java/oop/JavaIterator.java
@@ -0,0 +1,47 @@
+package hackerrank.java.oop;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaIterator {
+
+    static Iterator<Object> func(List<Object> myList) {
+
+        Iterator<Object> it = myList.iterator();
+
+        while (it.hasNext()) {
+            Object element = it.next();
+            if ("###".equals(element))
+                break;
+        }
+        return it;
+
+    }
+
+    public static void runCode() {
+        Scanner sc = new Scanner(System.in);
+        List<Object> mylist = new ArrayList<>();
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            mylist.add(sc.nextInt());
+        }
+
+        mylist.add("###");
+
+        for (int i = 0; i < m; i++) {
+            mylist.add(sc.next());
+        }
+
+        Iterator<Object> it = func(mylist);
+
+        while (it.hasNext()) {
+            Object element = it.next();
+            System.out.println(element);
+        }
+    }
+}
---
+++ src/test/java/hackerrank/java/oop/JavaIteratorTest.java
@@ -0,0 +1,35 @@
+package hackerrank.java.oop;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JavaIteratorTest {
+    private static final String IN = """
+            2 2
+            42
+            10
+            Hello
+            Java""";
+
+    private static final String OUT = """
+            Hello
+            Java""";
+
+    @BeforeAll
+    static void setUp() {
+        System.setIn(new ByteArrayInputStream(IN.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    void runCode() throws Exception {
+        String actual = tapSystemOut(JavaIterator::runCode);
+        assertEquals(OUT, actual.strip());
+    }
+}
